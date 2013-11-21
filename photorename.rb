92109ba31017c8ee41600320bf9ec8d95ede018d
Brian Foshee <brianfoshee@gmail.com>
2013-11-20 22:08:16 -0500

    beginning to update this. not production ready.

--- photorename.rb
+++ photorename.rb
@@ -1,37 +1,40 @@
-require 'rubygems'
-require 'RMagick'
-
 class RenamePhotos
+  require 'mini_magick'
+  include MiniMagick
+
   attr_accessor :base_folder, :destination_folder, :counter
+
   def initialize
-    @base_folder = "/path/to/folders/of/images"
-    @destination_folder = "/path/to/where/you/want/images"
+    @base_folder = "/Users/brian/Desktop/pic"
+    @destination_folder = "/Users/brian/Desktop/final"
     @counter = 1
   end
-  
+
   def write_data(day, time, image)
     postfix = 0
+    # 20101223-123415-0
+    # do some fancy strftime stuff instead of this fantastic array index formatting
     while (File.exists?("#{@destination_folder}/#{day[0]+day[1]+day[2]}-#{time[0]+time[1]+time[2]}-#{postfix}.jpg"))
       postfix = postfix + 1
     end
     file_name = "#{@destination_folder}/#{day[0]+day[1]+day[2]}-#{time[0]+time[1]+time[2]}-#{postfix}.jpg"
     image.write(file_name)
-    image.release!
     puts "Writing file number #{@counter}"
     @counter = @counter + 1
   end
-  
+
   def read_data(image)
-    datetime = image.get_exif_by_entry("DateTimeOriginal")[0][1].split(" ")
-    write_data(datetime[0].split(":"), datetime[1].split(":"), image)
+    d = Date.strptime image['EXIF:DateTimeOriginal'], "%Y:%m:%d %H:%M:%S"
+    write_data(d, image)
   end
-  
+
   def go
     puts "before Job: " + Dir["#{@destination_folder}/*"].count.to_s + " files"
-    Dir["#{@base_folder}/*"].each do |folder|
-      Dir["#{folder}/*"].each do |image|
-        read_data(Magick::Image.read("#{image}").first)
-      end
+    Dir["#{@base_folder}/*"].each do |image|
+      i = Image.open("#{image}")
+      puts i.path
+      read_data(i)
+      i.destroy!
     end
     puts "After Job: "+ Dir["#{@destination_folder}/*"].count.to_s + " files"
   end
